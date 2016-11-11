d7a248e6ff66dae1111d702620db24cbd01dcd31
B-raw <michaelbarry1989@hotmail.com>
2016-11-11 08:17:04 +0000

    Update specrunner to new structure

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ var noteList = new NoteList();
 var noteController = new NoteController(noteList);
 listenForFormSubmit();
 makeURLChangeShowNoteForCurrentPage();
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body", false);
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
